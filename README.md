e566d3f6370996c16ea2fe4aa4adf74e543e8fb7
Denise Chang <ddleletan@hotmail.com>
2024-03-26 18:30:50 -0400

    update to readme and simulation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# final_2
+# Analysis on Pokemon
+
+A short analysis on pokemon. This repository will be update to be pertinent to the future paper.
+
+## LLM USAGE
+No LLMs were used in the building of this repo.
--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,22 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the data on pokemon (to be updated)
+# Author: Denise Chang
+# Date: 26 March 2024
+# Contact: dede.chang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
-
+# EXAMPLE 
+data <-
+  tibble(
+    level = rnorm(n = 1000, mean = 100, sd = 10) |>
+    floor(),
+    weakness = sample(x = c('fire', 'not fire'), size = 1000, replace = TRUE)
+  )
 
 
