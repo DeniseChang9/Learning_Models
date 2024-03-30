283059d13911978aaba8b52d7d57a8e3d58a2216
Denise Chang <ddleletan@hotmail.com>
2024-03-30 15:44:32 -0400

    clean repo

---
+++ scripts/00-download_packages.R
@@ -0,0 +1,12 @@
+#### Preamble ####
+# Purpose: Installs the necessary packages used in this repository
+# Author: Denise Chang
+# Date: 30 March 2024
+# Contact: dede.chang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None
+
+#### Installs packages ####
+install.packages("tidyverse")
+install.packages("httr")
+install.packages("jsonlite")
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,25 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Denise Chang
+# Date: 28 March 2024
+# Contact: dede.chang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(httr)
+library(xml2)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+genshin_char <- GET("https://genshinlist.com/api/characters")
+genshin_weapons <- GET("https://genshinlist.com/api/weapons")
+genshin_artifacts <- GET("https://genshinlist.com/api/artifacts")
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
