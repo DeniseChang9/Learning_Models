fedf6096bf8db6fa81ec1b0b783229178ee622cb
Denise Chang <ddleletan@hotmail.com>
2024-03-31 00:32:44 -0400

    clean 2001 and 2006 data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,29 +1,27 @@
 #### Preamble ####
-# Purpose: Cleans the raw data
+# Purpose: Cleans the raw data of the 2001, 2006, 2011, 2016 and 2021 Canadian Language census
 # Author: Denise Chang
-# Date: 26 March 2024
+# Date: 30 March 2024
 # Contact: dede.chang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: None
+# Pre-requisites: 01-download_data.R
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
 library(arrow)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/raw_data.csv")
+#### Clean data for 2001 census ####
+census_2001 <- read_parquet("inputs/data/raw_data/2001_census.parquet")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(-timestamp) |>
-  rename(c(certainty = how_certain_about_your_guess_are_you, 
-           outcome = what_was_the_outcome)) |>
-  mutate(
-      outcome = if_else(outcome == "Guesse right",
-                        1,
-                        0))
- 
+# Remove demographic information
+clean_names(census_2001)
+
+census_2001 <-
+  census_2001 |>
+  slice(3:4)
+
+head(census_2001)
 
 #### Save data ####
-write_parquet(cleaned_data, "outputs/data/analysis_data.parquet")
+# write_parquet(cleaned_data, "outputs/data/analysis_data.parquet")
