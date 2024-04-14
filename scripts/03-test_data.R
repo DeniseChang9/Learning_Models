ef5b260ce5f3c7001af0fc3f2b5bcd0e24c8c4c4
Denise Chang <ddleletan@hotmail.com>
2024-04-13 22:33:49 -0400

    complete data testing

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,44 +1,47 @@
 #### Preamble ####
-# Purpose: Tests clean census data
+# Purpose: Tests clean test score data
 # Author: Denise Chang
 # Date: 31 March 2024
 # Contact: dede.chang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 02-data_cleaning.R
 
-
 #### Workspace setup ####
 library(tidyverse)
 library(arrow)
 
 #### Test data ####
+clean_data <- read_parquet("outputs/data/analysis_data.parquet")
 
-# read and select data
-clean_census_data <- read_parquet("outputs/data/clean_census_data.parquet")
-census_2001 <- filter(clean_census_data, year == 2001)
-census_2006 <- filter(clean_census_data, year == 2006)
-census_2011 <- filter(clean_census_data, year == 2011)
-census_2016 <- filter(clean_census_data, year == 2016)
-census_2021 <- filter(clean_census_data, year == 2021)
+# check that there are 2 possible subjects which are math and ela
+length(unique(clean_data$subject)) == 2
+all(clean_data$subject %in% c("math", "ela"))
 
-# check that the sum of respondents matches the total respondents in 2001, 2006, 2011, 2016 and 2021
-census_2001$total_knowledge_of_official_languages <= census_2001$english_only + census_2001$french_only + census_2001$english_and_french + census_2001$neither_english_nor_french
-census_2006$total_knowledge_of_official_languages == census_2006$english_only + census_2006$french_only + census_2006$english_and_french + census_2006$neither_english_nor_french
-census_2011$total_knowledge_of_official_languages == census_2011$english_only + census_2011$french_only + census_2011$english_and_french + census_2011$neither_english_nor_french
-census_2016$total_knowledge_of_official_languages >= census_2016$english_only + census_2016$french_only + census_2016$english_and_french + census_2016$neither_english_nor_french
-census_2021$total_knowledge_of_official_languages <= census_2021$english_only + census_2021$french_only + census_2021$english_and_french + census_2021$neither_english_nor_french
+# check that the data is within the studied range
+min(clean_data$year) >= 2016
+max(clean_data$year) <= 2021
 
-# check that the number of respondents are positive numbers
-only_respondents <-
-  clean_census_data |>
-  select(total_knowledge_of_official_languages, english_only, french_only, english_and_french, neither_english_nor_french)
+# check that the schooling method shares are whole with error
+schooling_shares <-
+  na.omit(clean_data) |>
+  select(share_inperson, share_virtual, share_hybrid)
 
-all(only_respondents >= 0)
+all(rowSums(schooling_shares, na.rm = TRUE) <= 1.0005)
+all(rowSums(schooling_shares, na.rm = TRUE) >= 0.9995)
 
-# check that every value in 'year' is unique
-num_values <-
-  clean_census_data$year |>
-  unique() |>
-  length()
+# check that the demographic shares are whole with error
+demographic_shares <- 
+  na.omit(clean_data) |>
+  select(share_black, share_white, share_hisp, share_other)
 
-nrow(clean_census_data) == num_values
+all(rowSums(demographic_shares, na.rm = TRUE) <= 1.0005)
+all(rowSums(demographic_shares, na.rm = TRUE) >= 0.9995)
+
+# check that all shares and rates are valid numbers
+num_data <-
+  na.omit(clean_data) |>
+  select(-subject, -year, -state_mailing)
+
+all(num_data <= 1)
+all(num_data >= 0)
+
