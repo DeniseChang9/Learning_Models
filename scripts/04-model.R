33d22fe1beddf3038e8b4e174d5c3d4ff10f89bd
Denise Chang <ddleletan@hotmail.com>
2024-04-15 15:55:19 -0400

    build model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,10 +1,10 @@
 #### Preamble ####
-# Purpose: Models whether we guessed right, based on our confidence
+# Purpose: Models the pass rates, based on the learning models
 # Author: Denise Chang
-# Date: 31 March 2024
+# Date: 15 April 2024
 # Contact: dede.chang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 02_download_data.R
+# Pre-requisites: 02-data_cleaning.R
 
 #### Workspace setup ####
 library(tidyverse)
@@ -12,23 +12,23 @@ library(arrow)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_parquet("outputs/data/clean_census_data.parquet")
+analysis_data <- read_parquet("outputs/data/analysis_data.parquet")
 
-analysis_data |>
-  ggplot(aes(x = certainty, y = outcome)) +
-  geom_jitter(height = 0)
-
+agg_data <-
+  analysis_data |>
+  mutate(pass_rate = (pass3 + pass4 + pass5 +pass6 + pass7 + pass8) / 6) |>
+  select(pass_rate, share_inperson, share_virtual, share_hybrid)
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = outcome ~ certainty,
-    data = analysis_data,
-    family = binomial(link = "logit"),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    formula = pass_rate ~ share_inperson + + share_virtual + share_hybrid,
+    data = agg_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+    seed = 302
   )
 
 summary(first_model)
