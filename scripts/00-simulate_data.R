0c937e6b31f053b15c426c4911a6a3d27f4563c8
Denise Chang <ddleletan@hotmail.com>
2024-04-17 04:29:52 -0400

    add pass_rate column

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -18,17 +18,15 @@ district_ids <- seq(1, num_districts)
 
 # Generate data
 data <- data.frame(
-  district_id = district_ids,                                 
-  year = sample(2017:2021, num_districts, replace = TRUE),    
-  in_person_share = runif(num_districts, min = 0, max = 1), 
-  virtual_share = runif(num_districts, min = 0, max = 1),     
+  district_id = district_ids,
+  year = sample(2017:2021, num_districts, replace = TRUE),
+  in_person_share = runif(num_districts, min = 0, max = 1),
+  virtual_share = runif(num_districts, min = 0, max = 1),
   hybrid_share = runif(num_districts, min = 0, max = 1),
+  pass_rate = runif(num_districts, min = 0, max = 1),
   subject = sample(c("ela", "math"), num_districts, replace = TRUE)
 )
 
 # Normalize shares so they sum up to 1 in each row
-data[, c("in_person_share", "virtual_share", "hybrid_share")] <- 
+data[, c("in_person_share", "virtual_share", "hybrid_share")] <-
   data[, c("in_person_share", "virtual_share", "hybrid_share")] / rowSums(data[, c("in_person_share", "virtual_share", "hybrid_share")])
-
-
-
