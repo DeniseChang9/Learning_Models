47fc689b09f063e54676e1cc8cebd020fbb93db1
Denise Chang <ddleletan@hotmail.com>
2024-04-17 01:49:56 -0400

    add more simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,12 +10,25 @@
 library(tidyverse)
 
 #### Simulate data ####
-# EXAMPLE 
-data <-
-  tibble(
-    level = rnorm(n = 500, mean = 50, sd = 4) |>
-    floor(),
-    weakness = sample(x = c('water', 'not water'), size = 500, replace = TRUE)
-  )
+set.seed(42)
+
+# Simulate 500 districts
+num_districts <- 500
+district_ids <- seq(1, num_districts)
+
+# Generate data
+data <- data.frame(
+  district_id = district_ids,                                 
+  year = sample(2017:2021, num_districts, replace = TRUE),    
+  in_person_share = runif(num_districts, min = 0, max = 1), 
+  virtual_share = runif(num_districts, min = 0, max = 1),     
+  hybrid_share = runif(num_districts, min = 0, max = 1),
+  subject = sample(c("ela", "math"), num_districts, replace = TRUE)
+)
+
+# Normalize shares so they sum up to 1 in each row
+data[, c("in_person_share", "virtual_share", "hybrid_share")] <- 
+  data[, c("in_person_share", "virtual_share", "hybrid_share")] / rowSums(data[, c("in_person_share", "virtual_share", "hybrid_share")])
+
 
 
